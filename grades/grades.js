5adcb4ffd8dc74890ba4a3dbe7f55cf2a46a524c
krysmathis <krysmathis@gmail.com>
2017-10-07 08:38:00 -0500

    var to let

--- grades/grades.js
+++ grades/grades.js
@@ -1,7 +1,7 @@
 const scores = [82, 71, 62, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87, 60, 75, 56]
 const grades = { "A": 0, "B": 0, "C":0,"D":0,"F":0} // You'll need to change this line of code
 
-var convertScoresToGrades = (function() {
+let convertScoresToGrades = (function() {
 
     for (let i = 0; i < scores.length; i++) {
         /*
