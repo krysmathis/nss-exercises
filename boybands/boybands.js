956d8dab82dd7853f30099380bb1b275c75c8229
krysmathis <krysmathis@gmail.com>
2017-10-10 12:43:56 -0500

    broke boybands into two loops

--- boybands/boybands.js
+++ boybands/boybands.js
@@ -7,20 +7,19 @@ const bandElement = document.getElementById("boy-bands");
 // Get a reference to the appropriate DOM element for vegetables
 const veggieElement = document.getElementById("vegetables");
 
-const maxArrayLength = (bands.length > vegetables.length) ? bands.length : vegetables.length;
-// Execute a for loop that will iterate over the arrays
-for (let loopTracker = 0; loopTracker < maxArrayLength; loopTracker += 1) {
 
+for (let loopTracker = 0; loopTracker < bands.length; loopTracker += 1) { 
   // Get a reference to the current item in the bands array
   const currentBand = bands[loopTracker];
 
   // Update the innerHTML value of the DOM element for bands
   bandElement.innerHTML += `<p>${currentBand}</p>`;
+}
 
-  // Get a reference to the current item in the vegetables array
+for (var loopTracker = 0; loopTracker < vegetables.length; loopTracker++) {
+  // // Get a reference to the current item in the veggies array
   const currentVeggie = vegetables[loopTracker];
-
+  
   // Update the innerHTML value of the DOM element for vegetables
   veggieElement.innerHTML += `<p>${currentVeggie}</p>`;
 }
-
