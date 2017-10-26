ef7a6691ff6b3f7de842aea0e9ceb214e556d111
krysmathis <krysmathis@gmail.com>
2017-10-26 15:25:00 -0500

    initial lexcopistan

---
+++ lexscopistan/scripts/heap-scope.js
@@ -0,0 +1,152 @@
+const gemHeapSkope = function () { // No parameter needed
+    // Resource contained inside
+
+
+    /*
+    The gem mine does not exist outside the barricade of the
+    hëap-skopes. The Lexscopistanians build the barricade
+    around their facility AND the resource.
+
+    a.k.a.
+    Instead of being located in an outer scope to the
+    function, the gem mine is enclosed by the scope of
+    the `gemHeapSkope` function.
+    */
+    const GemMine = {
+        "Onyx": {
+            "kilograms": 453
+        },
+        "Amethyst": {
+            "kilograms": 453
+        },
+        "Bloodstone": {
+            "kilograms": 453
+        },
+        "Emerald": {
+            "kilograms": 453
+        }
+    }
+
+    /*
+    Instead of processing the entirety of the resources in
+    bulk - which is what the stâck-skope does - this skope
+    will return an object that has a method for processing
+    each type of mineral.
+
+    We're exposing the functionality of this skope to code
+    in the outer scope, so that the order in which minerals
+    are processed can be customized.
+
+    Hëap-skopes workshops can process 5 kilograms of a
+    mineral with each work order. So every time the `process`
+    function is invoked, subtract 5 from the amount of the
+    requested mineral from the enclosed GemMine above.
+    */
+    return {
+        "process": function (requestedMineral) {
+            /*
+            Subtract 5 from the total kilograms available in
+            the gem mine, but make sure you stop when there
+            are no minerals left.
+            */
+            let gem = GemMine[requestedMineral];
+            let remainingKilos = gem.kilograms;
+            
+            let kilosToProcess = remainingKilos >= 5 ? 5 : remainingKilos;
+            gem.kilograms += -kilosToProcess;
+
+            return {
+                "mineral": requestedMineral,
+                "amount": kilosToProcess // Change this to the correct amount
+            }
+        }
+    }
+}
+
+/*
+The SkopeManager variable represents the object with the
+`process` method on it.
+*/
+const SkopeManager = gemHeapSkope()
+
+/*
+Create a generator for 30 storage containers, which is how many a hëap-skope
+is equipped with.
+*/
+const storageContainerGenerator = function* () {
+    let currentContainer = 0;
+    let maxContainers = 30;
+
+    while (currentContainer <= maxContainers){
+        yield { "id": currentContainer, "type": "Mineral", "orders": [] }
+        currentContainer++;
+    }
+};
+
+const storageContainerFactory = storageContainerGenerator();
+
+
+// a place to hold the collected containers
+let heapSkopeContainers = [];
+
+
+// get the first container and initialize a variable to represent it is empty
+let currentContainer = storageContainerFactory.next().value;
+let containerKilograms = 0;
+
+
+/*
+Process the gems in any order you like until there none
+left in the gem mine.
+*/
+const gemSequence = ["Onyx","Amethyst","Bloodstone","Emerald"];
+gemSequence.forEach(function(currentGem){
+    
+        /*
+            Once we've depleted a gem, it will return an amount of zero
+            Loop through each gem until you get an order with a zero.
+        */
+        // intialize an order with a phony amount
+        let order = { "amount": 1 }
+
+        while (order.amount > 0){
+            
+            order = SkopeManager.process(currentGem);
+
+            let orderNotZero = order.amount > 0;
+            let containerHasCapacity = containerKilograms + order.amount <= 565;
+
+            /*
+                Place the gems in the storage containers, making sure that
+                once a container has 565 kilograms of gems, you move to the
+                next one.
+            */
+
+            if (orderNotZero && containerHasCapacity) {
+                pushToContainer(order)
+
+            } else if (!containerHasCapacity) {
+                heapSkopeContainers.push(currentContainer);
+                currentContainer = storageContainerFactory.next().value;
+                containerKilograms = 0;
+                pushToContainer(order);
+            }
+        }   
+
+        function pushToContainer(order) {
+            currentContainer.orders.push(order);
+            containerKilograms += order.amount;
+        }
+       // }
+});
+
+// Clean-up any half full containers
+if (currentContainer.orders.length > 0) {
+    heapSkopeContainers.push(currentContainer);
+}
+
+console.log(heapSkopeContainers);
+
+
+
+
